628d050a3fd23ff44fc9977feb8ae2fd306d698a
nikita <kumbhareshubham9@gmail.com>
2021-04-25 00:58:55 +0530

    Third Commit

--- controllers/product.js
+++ controllers/product.js
@@ -10,12 +10,33 @@ exports.create = (req, res) => {
   form.parse(req, (err, fields, files) => {
     if (err) {
       return res.status(400).json({
-        error: err,
+        error: "Image could not be uploaded !!!",
+      });
+    }
+
+    const { name, description, price, category, quantity, shipping } = feild;
+
+    if (
+      !name ||
+      !description ||
+      !price ||
+      !category ||
+      !quantity ||
+      !shipping
+    ) {
+      return res.status(400).json({
+        error: "All feilds are required !!!",
       });
     }
 
     const product = new Product(feild);
     if (files.photo) {
+      console.log("Files Photo: ", files.photo);
+      if (files.photo.size > 1000000) {
+        return res.status(400).json({
+          error: "Image should be of size less than 1mb.",
+        });
+      }
       product.photo.data = fs.fileSync(files.photo.path);
       product.photo.contentType = files.photo.type;
     }
